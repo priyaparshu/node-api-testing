605d5f31586cfd5a013fd391b30711f267750abd
priyaparshu <priyaparshu@gmail.com>
2019-01-18 13:48:56 -0600

    add get /todos route

--- server/server.js
+++ server/server.js
@@ -41,7 +41,13 @@ app.post('/todos', (req, res) => {
 //     res.statusCode(401).send(err);
 // })
 
-
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 module.exports = { app };
 
 
