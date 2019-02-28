ac81ca582424f8ce7fad4ae8d834185683f40aa8
priyaparshu <priyaparshu@gmail.com>
2019-02-28 09:56:50 -0600

    Add tests for patch /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }]
 
 // beforeEach((done) => {
@@ -148,7 +150,66 @@ describe('DELETE /todos/:id', () => {
         request(app)
             .delete('/todos/123abc')
             .expect(404)
-            .end(done);
+            .end(done)
     });
 
 });
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var text = 'this should be the new text';
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text: text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(typeof (res.body.todo.completedAt)).toBe('number')
+                console.log(typeof (completedAt))
+            }).end(done);
+
+    })
+
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        hexId = todos[1]._id.toHexString();
+        var text = 'this should be the updated text';
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text: text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
+
+            }).end(done)
+
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+
+
+        it('should return 404 for non-objects ID', (done) => {
+            request(app)
+                .patch('/todos/123abc')
+                .expect(404)
+                .end(done)
+        })
+    })
+})
